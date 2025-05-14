f12ca3e6b4939d3d036c3b4349ed47507d02d8f3
Cristhian134 <cristhiansamir56@gmail.com>
2025-05-14 17:31:12 -0500

    Configuracion para produccion

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,13 +1,48 @@
 import dj_database_url
 from os import getenv, path
 from dotenv import load_dotenv
-from .base import *  # noqa
-from .base import BASE_DIR
 
-local_env_file = path.join(BASE_DIR, ".envs", ".env.production")
+from pathlib import Path
+
+# Importar nuevo
+from dotenv import load_dotenv
+from os import getenv, path
+from datetime import timedelta
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+# Antiguo
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+
+# Nuevo
+APPS_DIR = BASE_DIR / "core_apps"
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
 
 if path.isfile(local_env_file):
   load_dotenv(local_env_file)
+
+DJANGO_APPS = [
+  "django.contrib.admin",
+  "django.contrib.auth",
+  "django.contrib.contenttypes",
+  "django.contrib.sessions",
+  "django.contrib.messages",
+  "django.contrib.staticfiles",
+  "django.contrib.sites",
+  "django.contrib.humanize",
+]
+
+LOCAL_APPS = [
+  "core_apps.autenticacion",
+  "core_apps.common",
+  "core_apps.protegido.home",
+  "core_apps.protegido.crear_convocatoria",
+  "core_apps.protegido.listar_docentes",
+  "core_apps.protegido.ver_convocatorias",
+]
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
 
 DJANGO_MIDDLEWARES = [
   "django.middleware.security.SecurityMiddleware",
@@ -27,6 +62,34 @@ LOCAL_MIDDLEWARES = [
 
 MIDDLEWARE = DJANGO_MIDDLEWARES + LOCAL_MIDDLEWARES
 
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+  {
+    "BACKEND": "django.template.backends.django.DjangoTemplates",
+    "DIRS": [
+      BASE_DIR / 'core_apps/autenticacion/templates',
+      BASE_DIR / 'core_apps/common/templates',
+      BASE_DIR / 'core_apps/protegido/home/templates',
+      BASE_DIR / 'core_apps/protegido/crear_convocatoria/templates',
+      BASE_DIR / 'core_apps/protegido/listar_docentes/templates',
+      BASE_DIR / 'core_apps/protegido/ver_convocatorias/templates',
+    ],
+    "APP_DIRS": True,
+    "OPTIONS": {
+      "context_processors": [
+        "django.template.context_processors.request",
+        "django.contrib.auth.context_processors.auth",
+        "django.contrib.messages.context_processors.messages",
+      ],
+    },
+  },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+
+# Database
 
 DATABASES = {
   'default': dj_database_url.config(
@@ -36,6 +99,80 @@ DATABASES = {
   )
 }
 
+
+PASSWORD_HASHERS = [
+  "django.contrib.auth.hashers.Argon2PasswordHasher",
+  "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+  "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+  "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+  "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+  {
+    "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+  },
+  {
+    "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+  },
+  {
+    "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+  },
+  {
+    "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+  },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+SITE_ID = 1
+
+LOGIN_URL = 'login'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+# STATIC_URL = "/static/"
+# STATIC_ROOT = str(BASE_DIR / "staticfiles")
+
+STATIC_URL = '/static/'
+STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
+
+# Si deseas incluir archivos estáticos dentro de cada aplicación, asegúrate de tener:
+STATICFILES_DIRS = [
+  APPS_DIR / "autenticacion" / "static",
+  APPS_DIR / "common" / "static",
+  APPS_DIR / "protegido" / "home" / "static",
+  APPS_DIR / "protegido" / "crear_convocatoria" / "static",
+  APPS_DIR / "protegido" / "listar_docentes" / "static",
+  APPS_DIR / "protegido" / "ver_convocatorias" / "static",
+]
+
+# Esta configuración es para las aplicaciones dentro del proyecto (como 'autenticacion')
+STATICFILES_FINDERS = [
+  'django.contrib.staticfiles.finders.FileSystemFinder',
+  'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+AUTH_USER_MODEL = "common.Usuario"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 SECRET_KEY = getenv("SECRET_KEY")
 
