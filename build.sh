3c8c21fdacc1099cbb8e4e9e25f0a1a03fe7c6bc
Cristhian134 <cristhiansamir56@gmail.com>
2025-05-15 20:12:31 -0500

    Borrar migraciones antes de hacerlas

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ set -o errexit
 
 pip install -r ./requirements/production.txt
 
+echo "🔥 Borrando migraciones antiguas (excepto __init__.py)..."
+find core_apps/common/migrations/ -type f ! -name '__init__.py' -name '*.py' -delete
+find core_apps/common/migrations/ -type f -name '*.pyc' -delete
+
 python manage.py collectstatic --no-input
 
 python manage.py makemigrations
