bbb115d150be40e3678e073d6075fdf40e41c900
Cristhian134 <cristhiansamir56@gmail.com>
2025-05-21 20:51:17 -0500

    Formatear permisos

--- core_apps/permisos.py
+++ core_apps/permisos.py
@@ -10,18 +10,28 @@ MODULOS = [
     "nombre": "Listar docentes",
     "icono": "fa fa-list",
     "url": "/listar-docentes/",
-    "roles": [ROLES.ADMINISTRADOR, ROLES.DECANO],
+    "roles": [
+      ROLES.ADMINISTRADOR,
+      ROLES.DECANO
+    ],
   },
   {
     "nombre": "Crear convocatoria",
     "icono": "fa fa-bullhorn",
     "url": "/crear-convocatoria/",
-    "roles": [ROLES.ADMINISTRADOR, ROLES.DECANO, ROLES.ENCARGADO_CONSEJO],
+    "roles": [
+      ROLES.ADMINISTRADOR,
+      ROLES.DECANO,
+      ROLES.ENCARGADO_CONSEJO
+    ],
   },
   {
     "nombre": "Ver convocatorias",
     "icono": "fa fa-external-link",
     "url": "/ver_convocatorias/",
-    "roles": [ROLES.ADMINISTRADOR, ROLES.ENCARGADO_CONSEJO, ROLES.EVALUADOR],
+    "roles": [
+      ROLES.ADMINISTRADOR,
+      ROLES.ENCARGADO_CONSEJO,
+      ROLES.EVALUADOR],
   },
 ]
