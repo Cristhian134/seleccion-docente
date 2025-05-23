a85b67df2366c351222cc9aba759d4302ce006b0
Cristhian134 <cristhiansamir56@gmail.com>
2025-05-23 03:30:05 -0500

    Cambiar el titulo del template listar docentes

--- core_apps/protegido/listar_docentes/views.py
+++ core_apps/protegido/listar_docentes/views.py
@@ -34,11 +34,13 @@ def listar_docentes_view(request):
       Q(seccion__curso__nombreCurso__icontains=codigo_curso)
     ).order_by('-notaEvaluacion')[:5]
     docentes = formatear_docentes(evaluaciones)
+    curso = Curso.objects.get(codigoCurso=codigo_curso)
 
     return render(request, 'listar_docentes.html', {
       "url_volver": "/home",
       "docentes": docentes,
       "cursos": cursos,
+      "titulo": curso.nombreCurso.capitalize()
     })
 
   return render(request, 'listar_docentes.html', {
